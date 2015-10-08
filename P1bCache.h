99120b83f1962432a7d5dcd1e5be03eedeaf325e
JeanRusli <jean_rusli@yahoo.com>
2015-10-08 04:49:33 -0500

    Update P1bCache.h

--- P1bCache.h
+++ P1bCache.h
@@ -7,7 +7,7 @@
 #define BLOCK_SIZE 4
 #define LINE_SIZE 8192 
 
-// Block Sizes
+// Block Address Bits
 #define TAG 17
 #define INDEX 9
 #define OFFSET 6
@@ -21,7 +21,11 @@ typedef struct Block_* Block;
  * Cache constructor allocating blocks within.
  */
 Cache createCache(int cacheSize, int blockSize);
- 
+
+/*
+ * Read method to read the address passed and simulate cache behavior.
+ * Inputs the cache being used and dec, which is the address being read (integer).
+ */
 void read(Cache cache, int dec);
 
 /*
